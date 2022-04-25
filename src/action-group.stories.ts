d2bbb939b8f86fcf3f809233670b9e5b9953ebf9
Budi Irawan <deerawan@gmail.com>
2022-04-25 11:41:57 +1000

    configure storybook

---
+++ src/action-group.stories.ts
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/web-components';
+import { html } from 'lit-html';
+
+export default {
+  title: 'Components/ActionGroup',
+  component: 'bdg-action-grup',
+} as Meta;
+
+export const Default = () => html`<p>asik</p>`;
